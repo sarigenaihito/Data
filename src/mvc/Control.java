a86cd6e6e7933cb9d1e7da7df65606a76214c39c
sarigenai <sarigenai@hinonishi-pc>
2013-06-27 11:17:13 +0900

    Ver0.2

--- src/mvc/Control.java
+++ src/mvc/Control.java
@@ -6,5 +6,19 @@ public class Control {
 	ArrayList<Goods> goods = new ArrayList<Goods>();
 	ArrayList<Maker> maker = new ArrayList<Maker>();
 
+	public boolean checkGoodsId(int goodsId) {
+		return true;
+	}
 	
+	public boolean checkMakerId(int makerId) {
+		return true;
+	}
+	
+	public void insertMake() {
+		
+	}
+	
+	public void insertGoods() {
+		
+	}
 }
