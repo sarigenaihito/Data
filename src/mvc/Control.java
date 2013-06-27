70dd9b51508096d8cf80f9389fac27b8e5da97dc
sarigenai <sarigenai@hinonishi-pc>
2013-06-27 11:10:58 +0900

    Ver0.2

--- src/mvc/Control.java
+++ src/mvc/Control.java
@@ -1,5 +1,10 @@
 package mvc;
 
-public class Control {
+import java.util.ArrayList;
 
+public class Control {
+	ArrayList<Goods> goods = new ArrayList<Goods>();
+	ArrayList<Maker> maker = new ArrayList<Maker>();
+
+	
 }
